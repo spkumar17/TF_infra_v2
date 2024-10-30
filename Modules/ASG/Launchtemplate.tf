f841fa9d9f1974e67f7fa6fe6b249255bd8d8582
prasannakumar <prasannakumarsinganamalla@gmail.com>
2024-10-31 01:26:32 +0530

    commit all

--- Modules/ASG/Launchtemplate.tf
+++ Modules/ASG/Launchtemplate.tf
@@ -19,5 +19,34 @@ resource "aws_launch_template" "launch_template" {
     iam_instance_profile {
         name = var.aws_iam_instance_profile
     }
-    user_data = base64encode(templatefile("..Modules/ASG/userdata.sh", {db_instance_endpoint = var.db_instance_endpoint,username = var.username,password = var.password}))
+user_data = base64encode(<<-EOF
+    #!/bin/bash
+
+    # Update package repository and install Docker
+    sudo apt update -y
+    sudo apt install -y docker.io
+
+    # Enable Docker service on startup
+    sudo systemctl enable docker
+    sudo systemctl start docker
+
+    # Add the 'ubuntu' user to the Docker group (common for Ubuntu instances)
+    sudo usermod -aG docker $USER
+    sudo usermod -aG docker ssm-user
+
+
+    # Pull the latest version of the Docker image
+    sudo docker pull prasannakumarsinganamalla431/petclinic:8
+
+    # Run Docker container with environment variables (replace with your actual values from Terraform)
+    sudo docker run -d --name petclinic \
+    -e MYSQL_URL=jdbc:mysql://${var.db_instance_endpoint}/petclinic \
+    -e MYSQL_USER=${var.username} \
+    -e MYSQL_PASSWORD=${var.password} \
+    -p 80:80 \  # Update based on your app's port
+    prasannakumarsinganamalla431/petclinic:8
+
+EOF
+)
+
 }
