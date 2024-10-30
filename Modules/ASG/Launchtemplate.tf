eea83fa340e0537ee8c5f5f1d784e59d65b475f6
prasannakumar <prasannakumarsinganamalla@gmail.com>
2024-10-30 17:44:32 +0530

    commit all

--- Modules/ASG/Launchtemplate.tf
+++ Modules/ASG/Launchtemplate.tf
@@ -18,44 +18,21 @@ resource "aws_launch_template" "launch_template" {
         name =var.aws_iam_instance_profile
         }
     user_data = base64encode(<<-EOF
+              
                 #!/bin/bash
-                # # Update packages and install Docker
-                # sudo apt-get update -y
-                # sudo apt-get install -y docker.io
+                # Update packages and install Nginx
+                sudo apt-get update -y
+                sudo apt-get install -y nginx
 
-                # # Start Docker and enable it to start at boot
-                # sudo systemctl start docker
-                # systemctl enable docker
+                # Start and enable Nginx
+                sudo systemctl start nginx
+                sudo systemctl enable nginx
 
-                # # Add the current user to the Docker group
-                # sudo usermod -aG docker $USER
+                # Set up a basic HTML page
+                echo "<html><body><h1>Welcome to Nginx on EC2</h1></body></html>" | sudo tee /var/www/html/index.html
 
-                # # Pull the Docker image
-                # docker pull prasannakumarsinganamalla431/petclinic:8
-
-                # # Run the Docker container with environment variables for the RDS connection
-                # docker run -d --name petclinic_app -p 8080:8080 \
-                #     -e DB_HOST="${var.db_instance_endpoint}" \
-                #     -e DB_PORT="3306" \
-                #     -e DB_NAME="${var.database_name}" \
-                #     -e DB_USER="${var.username}" \
-                #     -e DB_PASSWORD="${var.password}" \
-                #     prasannakumarsinganamalla431/petclinic:8
-
-                #!/bin/bash
-    # Update packages and install Nginx
-    sudo apt-get update -y
-    sudo apt-get install -y nginx
-
-    # Start and enable Nginx
-    sudo systemctl start nginx
-    sudo systemctl enable nginx
-
-    # Set up a basic HTML page
-    echo "<html><body><h1>Welcome to Nginx on EC2</h1></body></html>" | sudo tee /var/www/html/index.html
-
-    # Restart Nginx to ensure changes are applied
-    sudo systemctl restart nginx
+                # Restart Nginx to ensure changes are applied
+                sudo systemctl restart nginx
                 EOF
             )
     
