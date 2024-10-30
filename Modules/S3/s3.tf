a884631ef3ce09093ce5e8196d4fc92726ae952c
prasannakumar <prasannakumarsinganamalla@gmail.com>
2024-10-30 17:13:23 +0530

    commit all

--- Modules/S3/s3.tf
+++ Modules/S3/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "vpc_flow_logs_bucket" {
-  bucket = "myvpcflowlogss-${var.environment}"
+  bucket = "myvpcflowlogss-akakaka"
 
   tags = {
     Name        = "VPC Flow Logs"
