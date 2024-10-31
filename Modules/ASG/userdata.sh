03f1f5647fafebb938129f1031ffd43a8d55d813
prasannakumar <prasannakumarsinganamalla@gmail.com>
2024-11-01 02:30:14 +0530

    commit all

--- Modules/ASG/userdata.sh
+++ Modules/ASG/userdata.sh
@@ -19,7 +19,8 @@ sudo docker pull prasannakumarsinganamalla431/petclinic:8
 sudo docker run -d --name petclinic \
 -e MYSQL_URL=jdbc:mysql://${db_instance_endpoint}/petclinic \
 -e MYSQL_USER=admin \
--e MYSQL_PASSWORD=Devops#21 \
+-e MYSQL_PASS=Devops#21 \
 -e MYSQL_ROOT_PASSWORD=Devops#21 \
 -p 80:80 \
 prasannakumarsinganamalla431/petclinic:8
+
